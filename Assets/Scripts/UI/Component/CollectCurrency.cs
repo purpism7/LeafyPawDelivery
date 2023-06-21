838e92c9900d970fd04ee4d77c31be41e1197de3
HanSeungjae <purpism@gmail.com>
2023-06-22 00:24:59 +0900

    datapath 수정.

--- Assets/Scripts/UI/Component/CollectCurrency.cs
+++ Assets/Scripts/UI/Component/CollectCurrency.cs
@@ -33,14 +33,26 @@ namespace UI.Component
             var rectTm = GetComponent<RectTransform>();
             if (!rectTm)
                 return;
-            
+
+            var wayPoint = _data.EndPos - _data.StartPos;
+            wayPoint += _data.EndPos;
+            wayPoint.x += 100f;
+
+            //wayPoint.x += 50f;
+
+            var wayPoints = new Vector3[3];
+            wayPoints[0] = _data.StartPos;
+            wayPoints[1] = wayPoint;
+            wayPoints[2] = _data.EndPos;
+
             Sequence sequence = DOTween.Sequence()
                 .SetAutoKill(false)
                 .Append(rectTm.DOMove(_data.StartPos, 0))
                 .AppendCallback(() => { Activate(); })
                 // .Append(rectTm.DOShakePosition(0.3f, 100f, 30))
                 // .Append(rectTm.SetEase(_data.JumpEase))
-                .Append(rectTm.DOMove(_data.EndPos, 1f).SetEase(_data.MoveEase))
+                .Append(rectTm.DOPath(wayPoints, 1f).SetEase(_data.MoveEase))
+                //.Append(rectTm.DOMove(_data.EndPos, 1f).SetEase(_data.MoveEase))
                 .Join(rectTm.DOShakeRotation(1.5f, 140f))
                 .OnComplete(() =>
                 {
