af77e97c30ec86a29f6b30b37ce659dcedbd2e70
HanSeungjae <purpism@gmail.com>
2023-06-22 01:01:10 +0900

    재화 수집 연출 수정중.

--- Assets/Scripts/UI/Component/CollectCurrency.cs
+++ Assets/Scripts/UI/Component/CollectCurrency.cs
@@ -34,15 +34,20 @@ namespace UI.Component
             if (!rectTm)
                 return;
 
-            var wayPoint = _data.EndPos - _data.StartPos;
-            wayPoint += _data.EndPos;
-            wayPoint.x += 100f;
+           
+            var wayPoint1 = _data.EndPos - _data.StartPos;
+            //wayPoint1.x += 200f;
+            //wayPoint1.y += 200f;
+
+            var wayPoint2 = _data.EndPos;
+            wayPoint2.x += 100f;
 
             //wayPoint.x += 50f;
 
             var wayPoints = new Vector3[3];
             wayPoints[0] = _data.StartPos;
-            wayPoints[1] = wayPoint;
+            wayPoints[1] = wayPoint1;
+            //wayPoints[2] = wayPoint2;
             wayPoints[2] = _data.EndPos;
 
             Sequence sequence = DOTween.Sequence()
@@ -51,7 +56,7 @@ namespace UI.Component
                 .AppendCallback(() => { Activate(); })
                 // .Append(rectTm.DOShakePosition(0.3f, 100f, 30))
                 // .Append(rectTm.SetEase(_data.JumpEase))
-                .Append(rectTm.DOPath(wayPoints, 1f).SetEase(_data.MoveEase))
+                .Append(rectTm.DOPath(wayPoints, 1f, PathType.CatmullRom).SetEase(_data.MoveEase))
                 //.Append(rectTm.DOMove(_data.EndPos, 1f).SetEase(_data.MoveEase))
                 .Join(rectTm.DOShakeRotation(1.5f, 140f))
                 .OnComplete(() =>
