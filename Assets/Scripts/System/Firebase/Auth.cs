e8b75d4fd756f6fe328e7cf711766ef99ed19296
HanSeungjae <purpism@gmail.com>
2023-07-02 23:49:57 +0900

    로그인 인증 수정.

--- Assets/Scripts/System/Firebase/Auth.cs
+++ Assets/Scripts/System/Firebase/Auth.cs
@@ -13,42 +13,38 @@ namespace GameSystem.Firebase
 
         public IEnumerator CoInit()
         {
-            UserId = PlayerPrefs.GetString("Auth_UserId");
+            UserId = PlayerPrefs.GetString("Auth_UserId", string.Empty);
             Debug.Log("UserId = " + UserId);
-            if(string.IsNullOrEmpty(UserId))
+            if(!string.IsNullOrEmpty(UserId))
+                yield break;
+
+            bool endLoad = false;
+
+            var auth = FirebaseAuth.DefaultInstance;
+            yield return auth?.SignInAnonymouslyAsync().ContinueWith(
+                task => {
+                    if (task.IsCanceled)
+                        return;
+
+                    if (task.IsFaulted)
+                        return;
+
+                    var result = task.Result;
+
+                    UserId = result.User.UserId;
+                    Debug.Log("UserId = " + UserId);
+                    PlayerPrefs.SetString("Auth_UserId", UserId);
+
+                    endLoad = true;
+                });
+
+
+            while (!endLoad)
             {
-                bool endLoad = false;
-
-                var auth = FirebaseAuth.DefaultInstance;
-                yield return auth?.SignInAnonymouslyAsync().ContinueWith(
-                    task => {
-                        if (task.IsCanceled)
-                        {
-                            Debug.LogError("SignInAnonymouslyAsync was canceled.");
-                            return;
-                        }
-                        if (task.IsFaulted)
-                        {
-                            Debug.LogError("SignInAnonymouslyAsync encountered an error: " + task.Exception);
-                            return;
-                        }
-
-                        var result = task.Result;
-
-                        UserId = result.User.UserId;
-
-                        PlayerPrefs.SetString("Auth_UserId", UserId);
-
-                        endLoad = true;
-                    });
-
-
-                while(!endLoad)
-                {
-                    yield return null;
-                }
-                //yield return new WaitUntil(() => endLoad);
+                yield return null;
             }
+
+            Debug.Log("End Load");
 
             Debug.Log(UserId);
         }
