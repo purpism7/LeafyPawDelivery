e0568b9ef2c1610a5b03b96528f63ac566c94fef
HanSeungjae <purpism@gmail.com>
2023-07-11 23:53:53 +0900

    동물 해금 조건 기능 구현.

---
+++ Assets/Scripts/Data/AnimalOpeCondition.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class AnimalOpeCondition
+{
+    public int Id = 0;
+    public long AnimalCurrency = 0;
+    public long ObjectCurrency = 0;
+    public int Cash = 0;
+}
