92f5bbd6ef7837296dbf3811e09cd8758bed8ba8
HanSeungjae <purpism@gmail.com>
2023-08-21 11:31:58 +0900

    GameScene 으로 바로 진입 시, 언어 ko 로 적용.

--- Assets/Scripts/Scene/Game.cs
+++ Assets/Scripts/Scene/Game.cs
@@ -19,6 +19,8 @@ namespace Scene
             {
                 if (eSceneType == Type.EScene.Game)
                 {
+                    LocalizationSettings.SelectedLocale = UnityEngine.Localization.Locale.CreateLocale("ko");
+
                     _preprocessing?.Init(this);
                 }
             }
