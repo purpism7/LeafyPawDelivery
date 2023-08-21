f27ceacfdc4f0b524d572ad315717de0a69b3092
HanSeungjae <purpism@gmail.com>
2023-08-21 11:33:26 +0900

    컴파일 에러 수정.

--- Assets/Scripts/Scene/Game.cs
+++ Assets/Scripts/Scene/Game.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using GameSystem;
 using UnityEngine;
+using UnityEngine.Localization.Settings;
 using UnityEngine.SceneManagement;
 
 namespace Scene
