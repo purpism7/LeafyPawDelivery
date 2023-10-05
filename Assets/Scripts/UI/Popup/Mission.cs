22e5b3cac632c6c12a5be701ceba94ad3791427a
sj.han <gs.purpism@kyowon.co.kr>
2023-10-05 16:07:32 +0900

    미션 추가.

---
+++ Assets/Scripts/UI/Popup/Mission.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UI
+{
+    public class Mission : BasePopup<Mission.Data>
+    {
+        public class Data : BaseData
+        {
+
+        }
+
+        public override void Initialize(Data data)
+        {
+            base.Initialize(data);
+        }
+
+        public override void Activate()
+        {
+            base.Activate();
+        }
+    }
+}
+
