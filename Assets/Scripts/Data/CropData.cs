d9d1cf82ea65476ec06e4e355a3ab174587c7386
Seungjae <sj@SJui-MacBookPro.local>
2025-12-30 00:14:16 +0900

    개화 시간 데이터 추가.

--- Assets/Scripts/Data/CropData.cs
+++ Assets/Scripts/Data/CropData.cs
@@ -5,6 +5,7 @@ using Newtonsoft.Json;
 [System.Serializable]
 public class CropData : Data.Base
 {
-    [JsonProperty("img_name")] 
-    public string ImgName { get; private set; } = string.Empty;
+    [JsonProperty("img_name")] public string ImgName { get; private set; } = string.Empty;
+
+    [JsonProperty("bloom_time")] public int BloomTime { get; private set; } = 0;
 }
